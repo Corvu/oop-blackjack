9862eb60a1c389cebddbd7c9f747db0280ce2aeb
magi00 <56919394+magi00@users.noreply.github.com>
2020-03-28 15:42:56 +0200

    Update Mängija.java

--- src/Mängija.java
+++ src/Mängija.java
@@ -1,2 +1,15 @@
 public class Mängija {
+      public static void näida_mängu(){
+
+    }
+    public static void näida_peamenüü(){
+        System.out.println("Tere tulemast, mängusse blackjack!");
+        System.out.println("Mängi arvutiga ja proovi oma õnne.");
+        Scanner s= new Scanner(System.in);
+        System.out.println("Kui soovid alustada mängu sisesta 'start':");
+        String sisestatud=s.nextLine();
+        if (sisestatud=="start"){
+            näida_mängu();
+        }
+    }
 }
