c01fb7eebeedf729229d9d91c407da7c822d68e3
magi00 <56919394+magi00@users.noreply.github.com>
2020-03-28 17:08:20 +0200

    Update Mängija.java

--- src/Mängija.java
+++ src/Mängija.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 public class Mängija {
+      public static int näida_lauda(kaardidMängija, kaardidDiiler){
+        for (int i = 0; i < kaardidMängija.lenght; i++) {
+            System.out.print("|"+kaardidMängija[i]+"|   ")
+        }
+        Scanner s=new Scanner(System.in);
+        System.out.println(" Kui vajutatud 1, võtta veel kaardi; kui 2, hoida: ");
+        int valik=(int) valueof(s.nextLine());
+        return valik;
+    }
       public static void näida_mängu(){
 
     }
