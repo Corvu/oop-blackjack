58a9bb483c2a001ed5d6a46c5ccf57a8e860335d
magi00 <56919394+magi00@users.noreply.github.com>
2020-03-31 12:09:14 +0300

    Update Mängija.java

--- src/Mängija.java
+++ src/Mängija.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Mängija {
+      //näitab mängijale kaardiid, mis tal on ja küsib otsust
       public static int näidaLauda(ArrayList<String> kaardidMängija, ArrayList<String> kaardidDiiler){
         for (int i = 0; i < kaardidMängija.size(); i++) {
             System.out.print("|"+kaardidMängija.get(i)+"|   ");
@@ -11,6 +12,7 @@ public class Mängija {
         int valik=s.nextInt();
         return valik;
     }
+      //väljastab mängija ja diileri punktid ning teatab, kes on võitja 
       public static void näida_tulemus(int võit, int summaMängija, int summaDiiler){
           System.out.println("Sinu tulemus: "+summaMängija);
           System.out.println("Diileri tulemus: "+summaDiiler);
@@ -25,6 +27,7 @@ public class Mängija {
           }
 
     }
+      //Tervitab mängijat ja pakkub alustada mängu
    public static int näida_peamenüü(){
         System.out.println("Tere tulemast, mängusse blackjack!");
         System.out.println("Mängi arvutiga ja proovi oma õnne.");
