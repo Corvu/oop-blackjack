0c8690b1e5649768b0eb02073bf762186256a2d6
magi00 <56919394+magi00@users.noreply.github.com>
2020-05-01 17:27:47 +0300

    Update MängijaGraafiline.java

--- src/main/java/MängijaGraafiline.java
+++ src/main/java/MängijaGraafiline.java
@@ -32,6 +32,45 @@ public class MängijaGraafiline implements KasutajaLiides{
 
     @Override
     public int näidaPeamenüü() {
-        return 0;
+         Text text=new Text();
+        text.setText("          Tere tulemast, mängusse blackjack!\n" +
+                     "          Mängi arvutiga ja proovi oma õnne.\n " +
+                     "Kui soovid alustada mängu, siis vajuta 'Start' nuppu.");
+        text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+        text.setY(130);
+        text.setX(90);
+
+
+        BorderPane root=new BorderPane();
+
+        Scene peamenüü = new Scene(root,600,335);  // luuakse stseen
+
+        root.setCenter(text);
+
+
+
+        final int[] i = new int[1];
+
+        Button nupp1 = new Button("Start"); // luuakse nupp
+        nupp1.setPrefWidth(75);
+        root.setBottom(nupp1);
+        nupp1.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                i[0] = 1;
+            }
+        });
+
+        Button nupp2=new Button("Loobu");
+        nupp2.setPrefWidth(75);
+        root.setRight(nupp2);
+
+        nupp2.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                i[0]=0;
+            }
+        });
+        return i[0];
     }
 }
